d5a6457b808f71cf86e91d5e02a80a0c70a637d1
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-20 02:18:00 -0300

    Troca de cor da borda e ícone ao focar input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -21,7 +21,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logImg} alt="Go Barber" />
 
-        <Form initialData={{ name: 'FirstName' }} onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
