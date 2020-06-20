9458f5e98c804095d9df529cbc3565d1e2bb0f20
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-20 02:34:09 -0300

    handleInputFocus com useCallback

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -29,6 +29,10 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
   useEffect(() => {
     registerField({
       name: fieldName,
@@ -41,7 +45,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     <Container isFilled={isFilled} isFocused={isFocused}>
       {Icon && <Icon size={20} />}
       <input
-        onFocus={() => setIsFocused(true)}
+        onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
