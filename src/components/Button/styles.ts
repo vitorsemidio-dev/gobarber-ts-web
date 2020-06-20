254eb4dcbcef2519dd8bff10a1df159b0c9d8391
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-20 00:41:21 -0300

    Estilos dos components Button e Input

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.button``;
+export const Container = styled.button`
+  background: #ff9000;
+  height: 56px;
+  border-radius: 10px;
+  border: 0;
+  padding: 0 16px;
+  color: #321e38;
+  margin-top: 16px;
+  width: 100%;
+  font-weight: 500px;
+  transition: background-color 0.2;
+
+  &:hover {
+    background: ${shade(0.2, '#ff9000')};
+  }
+`;
