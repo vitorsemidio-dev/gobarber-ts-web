611a1801320ab828165c2057eb607d2212ec2026
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-21 14:27:12 -0300

    Chamando rota sessions

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -14,13 +14,18 @@ import Input from '../../components/Input';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
   const { signIn } = useContext(AuthContext);
 
   const handleSubmit = useCallback(
-    async (data: any) => {
+    async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({});
 
@@ -35,7 +40,10 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        signIn();
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
       } catch (err) {
         const erros = getValidationErrors(err);
 
