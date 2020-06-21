df4dcf54b4f93d09b207f2fa0dd1c8bfe30a02aa
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-21 14:52:57 -0300

    Refatoração AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,6 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 
 import api from '../services/api';
-
-interface SignInCredentials {
-  email: string;
-  password: string;
-}
 
 interface AuthContextData {
   user: any;
@@ -17,11 +12,14 @@ interface AuthState {
   user: any;
 }
 
-export const AuthContext = createContext<AuthContextData>(
-  {} as AuthContextData,
-);
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
 
-export const AuthProvider: React.FC = ({ children }) => {
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
@@ -53,3 +51,15 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
