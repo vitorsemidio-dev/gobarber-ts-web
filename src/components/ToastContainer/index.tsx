6a0e4fbae8118be7cb3564af8e8d4a5418d3ad0f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-21 23:14:47 -0300

    Criando componente de Toast

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,43 +1,20 @@
-import React, { useCallback } from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
+import React from 'react';
 
-import { ToastMessage, useToast } from '../../hooks/toast';
+import Toast from './Toast';
 
-import { Container, Toast } from './styles';
+import { ToastMessage } from '../../hooks/toast';
+
+import { Container } from './styles';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
-  const handleRemoveToast = useCallback(
-    (id: string) => {
-      removeToast(id);
-    },
-    [removeToast],
-  );
-
   return (
     <Container>
       {messages.map((message) => (
-        <Toast
-          key={message.id}
-          type={message.type}
-          hasDescription={!!message.description}
-        >
-          <FiAlertCircle size={20} />
-
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button onClick={() => handleRemoveToast(message.id)} type="button">
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} message={message} />
       ))}
     </Container>
   );
