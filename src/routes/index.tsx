2033a0076132cf4a683512e54cac9a049a57061d
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-22 12:55:51 -0300

    Criação RouteComponent e lógica de autenticação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,10 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Dashboard from '../pages/Dashboard';
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} />
   </Switch>
 );
 
