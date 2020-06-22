55f5e013a1ec7e59f487938e6c31059b34a6dce1
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-21 22:22:27 -0300

    implementando addToast

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useCallback } from 'react';
+import React, { createContext, useContext, useCallback, useState } from 'react';
+import { uuid } from 'uuidv4';
 
 import ToastContainer from '../components/ToastContainer';
 
@@ -7,12 +8,33 @@ interface ToastContextData {
   removeToast(): void;
 }
 
+interface ToastMessage {
+  id: string;
+  type?: 'success' | 'error' | 'info';
+  title: string;
+  description?: string;
+}
+
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 
 const ToastProvider: React.FC = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('addToast');
-  }, []);
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
+
+  const addToast = useCallback(
+    ({ description, title, type }: Omit<ToastMessage, 'id'>) => {
+      const id = uuid();
+
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+
+      setMessages([...messages, toast]);
+    },
+    [messages],
+  );
   const removeToast = useCallback(() => {
     console.log('removeToast');
   }, []);
