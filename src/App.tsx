de76f3616ed0a878f797338c9ad40b1a19eb9e24
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-06-22 12:13:39 -0300

    Configuração de rotas e ajustes nos links

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
+import AppProvider from './hooks';
+import Routes from './routes';
 
 import GlobalStyle from './styles/global';
 
-import AppProvider from './hooks';
-
 const App: React.FC = () => (
-  <>
+  <BrowserRouter>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
-
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
 import { useToast } from '../../hooks/toast';
@@ -82,13 +83,13 @@ const SignIn: React.FC = () => {
 
           <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
+          <Link to="forgot">Esqueci minha senha</Link>
         </Form>
 
-        <a href="create">
+        <Link to="signup">
           <FiLogIn />
           Criar Conta
-        </a>
+        </Link>
       </Content>
 
       <Background />
