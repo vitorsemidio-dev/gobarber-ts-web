ab9a573c1459d907f23bb07bf229d9e5662426a7
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-12 22:29:09 -0300

    Estrutura Header Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, Profile } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="Go Barber" />
+        </HeaderContent>
+
+        <Profile>
+          <img
+            src="https://api.adorable.io/avatars/50/abott@adorable.png"
+            alt="Avatar"
+          />
+
+          <div>
+            <span>Bem-vindo,</span>
+            <strong>Usuário GoBarber</strong>
+          </div>
+        </Profile>
+
+        <button type="button">
+          <FiPower />;
+        </button>
+      </Header>
+    </Container>
   );
 };
 
