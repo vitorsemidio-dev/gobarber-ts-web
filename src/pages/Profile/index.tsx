1366a2b7ffd1e6892f2a3eae0f0e9a67b9cdfe6e
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-13 12:07:29 -0300

    Fix: atualização profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -61,7 +61,29 @@ const Profile: React.FC = () => {
           abortEarly: false,
         });
 
-        await api.put('/profile', data);
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? {
+                old_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
 
         history.push('/dashboard');
 
