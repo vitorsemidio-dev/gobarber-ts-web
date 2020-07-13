c6c2dd9e3dfb9deb241e9c4eb3b6868a0a71d6b6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-13 10:27:52 -0300

    containerStyle

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -81,6 +81,7 @@ const Profile: React.FC = () => {
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
           <Input
+            containerStyle={{ marginTop: 24 }}
             name="old_password"
             icon={FiLock}
             type="password"
