99d1a2879f88b6b3f0daada82025a88f20fbd288
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-13 10:25:52 -0300

    Componente Profile

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,109 @@
+import React, { useCallback, useRef } from 'react';
+import { FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { useHistory } from 'react-router-dom';
+
+import { useToast } from '../../hooks/toast';
+
+import api from '../../services/api';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Content } from './styles';
+
+interface ProfileFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: ProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório'),
+          email: Yup.string()
+            .required('E-mail é obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado',
+          description: 'Você já pode fazer seu logon no GoBarber',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const erros = getValidationErrors(err);
+
+          formRef.current?.setErrors(erros);
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro no cadastro',
+          description: 'Ocorreu um erro ao fazer cadastro. Tente novamente',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <Content>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Meu perfil</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="old_password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha atual"
+          />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Nova senha"
+          />
+          <Input
+            name="password_confirmation"
+            icon={FiLock}
+            type="password"
+            placeholder="Confirmar senha"
+          />
+
+          <Button type="submit">Confirmar mudanças</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
