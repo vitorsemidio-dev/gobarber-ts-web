9e670cbe180e74fbec61e81920dfe0fd5cc07df0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-15 12:54:51 -0300

    [Test]: Estilos ao realizar focus no Input

--- src/__tests__/components/Input.spec.tsx
+++ src/__tests__/components/Input.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 
 import Input from '../../components/Input';
 
@@ -23,5 +23,21 @@ describe('Input component', () => {
     );
 
     expect(getByPlaceholderText('E-mail')).toBeTruthy();
+  });
+
+  it('should render hightlight on input focus', async () => {
+    const { getByPlaceholderText, getByTestId } = render(
+      <Input name="email" placeholder="E-mail" />,
+    );
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    await wait(() => {
+      expect(containerElement).toHaveStyle('border-color: #ff9000');
+      expect(containerElement).toHaveStyle('color: #ff9000');
+    });
   });
 });
