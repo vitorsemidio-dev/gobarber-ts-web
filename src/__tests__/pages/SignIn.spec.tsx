5671a2003f81d84b6ed388bf21d238e95df925bd
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-15 00:23:43 -0300

    collectCoverageFrom

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import SignIn from '../../pages/SignIn';
 import { render } from '@testing-library/react';
+import SignIn from '../../pages/SignIn';
 
 jest.mock('react-router-dom', () => {
   return {
     useHistory: jest.fn(),
-    Link: ({ children }: { children: React.ReactNode }) => children
-  }
-})
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const {debug} = render(<SignIn />)
+    const { debug } = render(<SignIn />);
 
     debug();
-  })
-})
+  });
+});
