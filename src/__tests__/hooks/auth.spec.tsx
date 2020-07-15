5609ab3f0ae64e5377d2fa765129356ce993a2cb
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-07-15 13:43:21 -0300

    [Test]: GREEN SignIn comuseAuth e axios mock adapter

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,10 +1,23 @@
 import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
 
+import api from '../../services/api';
 import { useAuth, AuthProvider } from '../../hooks/auth';
 
+const apiMock = new MockAdapter(api);
+
 describe('Auth hook', () => {
-  it('should be able to sign in', () => {
-    const { result } = renderHook(() => useAuth(), {
+  it('should be able to sign in', async () => {
+    apiMock.onPost('sessions').replyOnce(200, {
+      user: {
+        id: 'id-uuid',
+        name: 'John Doe',
+        email: 'johndoe@mail.com',
+      },
+      token: 'token-jwt',
+    });
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
@@ -12,6 +25,8 @@ describe('Auth hook', () => {
       email: 'johndoe@mail.com',
       password: 'asdfasdf',
     });
+
+    await waitForNextUpdate();
 
     expect(result.current.user.email).toBe('johndoe@mail.com');
   });
